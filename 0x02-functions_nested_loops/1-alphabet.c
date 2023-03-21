d7673fb1ba0e0149226afca6f1cf6af62e37fddc
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-21 21:03:51 +0000

    print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - printing out alphabets in lowercase 
+ * by thenoskshade
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	char nosk;
+
+	for (nosk = 'a'; nosk <= 'z'; nosk++)
+	{
+		_putchar(nosk);
+	}
+	-putchar('\n');
+}
