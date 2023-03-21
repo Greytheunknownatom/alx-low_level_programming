380cbaf7b6b814990a7532b5cd01016c10d33afe
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-21 21:41:06 +0000

    prints x10 lowercase alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets x10 in lowercase
+ * by thenoskshade
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int nosk;
+	int nosk2;
+
+	for (nosk = 0; nosk <= 9; nosk++)
+	{
+		for (nosk2 = 'a' nosk2 <= 'z'; nosk2++)
+		{
+			_putcha(nosk2);
+		}
+		-putchar('\n');
+		return (0);
+	}
+}
