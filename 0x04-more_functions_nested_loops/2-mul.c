877727c569e8005cf13f40cbbc1067012242db56
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-24 16:17:45 +0000

    functions and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function to multiply 2 int
+ * @a: first int to be checked 
+ * @b: second int to be checked
+ * Return: addition of the both char
+ * noskshading
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
