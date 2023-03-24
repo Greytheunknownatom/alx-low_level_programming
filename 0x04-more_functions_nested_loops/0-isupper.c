8b0c35c2f17b2ccac1d3f374754c5045a28570ee
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-24 00:20:34 +0000

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * noskshading
+ * @c:to be checked
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+*/
+int _isupper(int c)
+{
+	if (isnosk(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
