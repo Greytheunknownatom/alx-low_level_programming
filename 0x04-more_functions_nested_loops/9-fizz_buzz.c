4f026159f961cc5f51312ad80e3e75168fb51d98
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-25 01:32:27 +0000

    functions and loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - prints the numbers from 1 to 100, followed by a new line 
- * but for multiples of three prints Fizz instead of the number 
- * and for the multiples of five prints Buzz
- * nosking
- * Return: (0)
+ * main - function that prints the numbers from 1 to 100, followed by a
+ * new line and for the multiples of five prints Buzz
+ * Return: 0
+ * noskshading
 */
 int main(void)
 {
