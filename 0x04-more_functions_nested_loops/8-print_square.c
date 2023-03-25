ef227d52bc87d8510188aa5065739e3f27af246c
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-25 00:59:49 +0000

    functions and loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square, followed by a new line;
+ * @size: size of the square
+ * noskshading
+*/
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int l, o;
+
+		for (l = 0; l < size; l++)
+		{
+			for (o = 0; i < size; o++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
