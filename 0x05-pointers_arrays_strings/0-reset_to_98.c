91b1757475c809af5e05b1d000ac4f38cc75fa99
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-27 18:04:17 +0000

    pointer points to int value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function would update the value of a var using
+ * pointer n
+ * @n: int to update
+ * noskshading
+ * Return: 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
