1e010f85f30df59b31505a0b09320f5dff3ae972
Greytheunknownatom <thenoskshade@gmail.com>
2023-04-18 04:01:14 +0000

    fist project on creating a new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog - this is a new type "struct dog" with the basic info of a dog
 * @name:char
@@ -12,11 +13,10 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /*dog_t - the typedef for struct dog*/
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
